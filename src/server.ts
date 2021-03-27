cf8ec752df90e952a332e40f832040211fc6b89d
Dimitrios Panteleimon Giakatos <dgiakatos@csd.auth.gr>
2021-03-27 20:04:57 +0200

    Uploading files work

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import express from 'express';
 import cors from 'cors';
-import multer from 'multer';
 import { v4 as uuidv4 } from 'uuid';
 import fs from 'fs';
 import path from 'path';
-import { fileFilter } from './helpers/file-filter'
+import Formidable from 'formidable';
 
 const app = express();
 
@@ -12,43 +11,34 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-const storage = (sessionId: string) => {
-    return multer.diskStorage({
-        destination: (req, file, cb) => {
-            const dir = path.join('uploads', sessionId, '');
-            fs.access(dir, noExist => {
-                if (noExist) {
-                    return fs.mkdir(dir, err => cb(err, dir));
-                }
-                return cb(null, dir);
-            });
-        },
-        filename: (req, file, cb) => {
-            cb(null, file.originalname);
-        }
-    })
-};
-
 app.get('/', (req, res) => {
     res.send('Welcome to WearMerge!');
 });
 
-app.post('/upload-files', (req, res) => {
+app.post('/upload-file', (req, res) => {
     const sessionId = uuidv4();
-
-    const upload = multer({ storage: storage(sessionId), fileFilter: fileFilter}).array('file');
-
-    upload(req, res, (err: any) => {
-        console.log(req.files);
-        if (!req.files) {
-            return res.send('Please select files to upload');
-        } else if (err instanceof multer.MulterError) {
-            return res.send(err);
-        } else if (err) {
-            return res.send(err);
-        }
-        res.send('Files uploaded with session id: ' + sessionId);
+    let form = new Formidable.IncomingForm({
+        uploadDir: 'tmp/'
     });
+    form.on('file', (filename, file) => {
+        const dir = path.join('uploads', sessionId, filename);
+        fs.access(dir, fs.constants.F_OK, (err) => {
+            if (err) {
+                fs.mkdir(dir, { recursive: true }, (err) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    fs.rename(file.path, path.join(dir, file.name), () => {});
+                });
+            } else {
+                fs.rename(file.path, path.join(dir, file.name), () => {});
+            }
+        });
+    });
+    form.on('end', () => {
+        res.status(200).send(sessionId);
+    });
+    form.parse(req, () => {});
 });
 
 app.listen(port, () => {
